45a03eb3d12e5a0deb35f227eb38ffc74def29ee
jackwenyoung <dlwxxxdlw@gmail.com>
2024-12-10 13:00:02 +0800

    finish the tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,10 @@ import { defineConfig } from "astro/config";
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [sentry(), spotlightjs()],
+	integrations: [sentry(), spotlightjs(), preact()],
+	site: "https://jack-learn-astro.netlify.app",
 });
